5836c19c6b283474efc140ba566d6e20543a50aa
mp <mpasdziernik@cognesys.de>
2016-08-26 12:19:02 +0200

    MySQL does not provide ddl rollback.

--- src/auth/db/schema.clj
+++ src/auth/db/schema.clj
@@ -8,11 +8,10 @@
   {:adapter (cj-adapter/hugsql-adapter-clojure-jdbc)})
 
 (defn mysql-create-tables [conn]
-  (jdbc/atomic conn
-    (mysql-create-users-table conn)
-    (mysql-create-tenant-table conn)
-    (mysql-create-tenant-user-table conn)
-    (mysql-create-role-table conn)
-    (mysql-create-capability-table conn)
-    (mysql-create-role-right-table conn)
-    (mysql-create-tenant-user-role-table conn)))
+  (mysql-create-users-table conn)
+  (mysql-create-tenant-table conn)
+  (mysql-create-tenant-user-table conn)
+  (mysql-create-role-table conn)
+  (mysql-create-capability-table conn)
+  (mysql-create-role-capability-table conn)
+  (mysql-create-tenant-user-role-table conn))
